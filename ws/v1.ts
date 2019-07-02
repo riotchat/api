431a23261f541b3ec611d530e8df3707bcc8bc44
Paul Makles <paulmakles@gmail.com>
2019-07-02 15:53:04 +0100

    Change the way messages are sent

--- ws/v1.ts
+++ ws/v1.ts
@@ -1,20 +1,13 @@
 import { FriendType } from '../v1/users';
-import { Message, MessageID } from '../v1/channels';
+import { Message as IMessage } from '../v1/channels';
 
 export interface Error {
 	type: 'error',
 	error: string
 };
 
-export interface MessageCreate extends Message {
-	type: 'messageCreate'
-};
-
-export interface MessageUpdate {
-	type: 'messageUpdate'
-	id: MessageID
-	content: string
-	updatedAt: number
+export interface Message extends IMessage {
+	type: 'message'
 };
 
 export interface Authenticated {
@@ -29,7 +22,7 @@ export interface UserUpdate {
 	relation?: FriendType
 }
 
-export type Packets = Error | MessageCreate | MessageUpdate | Authenticated | UserUpdate;
+export type Packets = Error | Message | Authenticated | UserUpdate;
 
 export interface Authenticate {
 	type: 'authenticate',
