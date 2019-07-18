b96a7406cde0e0accc71667910d0b187f3075d16
Paul Makles <paulmakles@gmail.com>
2019-07-18 10:39:09 +0100

    Add new errors.

--- v1/errors.ts
+++ v1/errors.ts
@@ -1,18 +1,34 @@
 export enum APIError {
     /**
-     * CATCH-ALL
+     * GENERAL ERRORS
      */
-    FAIL = 0x1
+    FAIL = 0x0,
+    MISSING_FIELDS = 0x1,
     /**
      * AUTHENTICATION ERRORS
      */
+    INVALID_EMAIL = 0x10,
+    INVALID_PASSWORD = 0x11,
+    INVALID_2FA = 0x12
 };
 
 const ERRORS: { [key in APIError]: [number, string] } =
 {
-    [0x1]: [500, 'Unknown error occurred!']
+    // GENERAL ERRORS
+    [0x0]: [500, 'Unknown error occurred!'],
+    [0x1]: [422, 'Missing fields.'],
+    // AUTH ERRORS
+    [0x10]: [403, 'Invalid email.'],
+    [0x11]: [403, 'Invalid password.'],
+    [0x12]: [403, 'Failed to verify 2FA.']
 };
 
 export function GetError(error: APIError) {
     return ERRORS[error];
+}
+
+export function SendError(res, error: APIError, details?: string) {
+    let [ status, reason ] = GetError(error);
+    res.status(status);
+    res.send({ error, reason, details });
 }
